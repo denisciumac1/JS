1587a65484c3665bf988f08e7a4cf2a94828d62d
denisciumac1 <43856379+denisciumac1@users.noreply.github.com>
2019-03-14 20:01:16 +0200

    Add files via upload

--- Garden2/garden.js
+++ Garden2/garden.js
@@ -3,15 +3,20 @@ const EMPTY  =    0;
 const CUCUMBER  = 1;
 const MELON  =    2;
 const TOMATO =    3;
+const RAT    =    4;
 
 const CUCUMBER_PRICE  = 10;
 const MELON_PRICE  =    20;
 const TOMATO_PRICE =    30;
 
 var money = 100;
+// coordonate rat
+var rat_r = 0;
+var rat_c = 0;
+var rat_dir= "right";
 
 var garden = [
-    [1,0,0,0,0,0,0,0,0,0],
+    [4,0,0,0,0,0,0,0,0,0],
     [1,0,0,0,0,3,0,0,0,0],
     [1,0,2,0,0,0,1,0,1,0],
     [0,0,0,0,0,1,0,0,0,0],
@@ -23,6 +28,45 @@ var garden = [
     [0,0,3,0,1,0,0,0,2,0],
 
    ];
+// mishcarile ratului
+function moveRatright(){
+  if(rat_c<9){
+  garden[rat_r][rat_c]=EMPTY;
+  garden[rat_r][++rat_c]=RAT;
+  show();
+  }
+}
+function moveRatleft(){
+  garden[rat_r][rat_c]=EMPTY;
+  garden[rat_r][--rat_c]=RAT;
+  show();
+}
+function moveRattup(){
+  garden[rat_r][rat_c]=EMPTY;
+  garden[--rat_r][rat_c]=RAT;
+  show();
+}
+function moveRatdown(){
+  garden[rat_r][rat_c]=EMPTY;
+  garden[++rat_r][rat_c]=RAT;
+  show();
+}
+function moveRat(){
+//schimbarea directie aliatoriu
+  if(Math.round(Math.random()*100)%5==0){
+  rat_dir= randomDirection();
+}
+  if(rat_dir=="right"){
+    moveRatright();
+  }
+}
+
+function randomDirection(){
+  var directions = ["right","left","up","down"];
+  return directions[Math.round(Math.random()*3)];
+}
+
+setInterval(moveRat, 100);
 
 function show(){
   var div = document.getElementById('garden');
@@ -38,8 +82,10 @@ function show(){
     div.innerHTML+= `<div class="cucumber" onclick="gather(CUCUMBER,${r},${c});"></div> `
       }else if(garden[r][c]== EMPTY) {
     div.innerHTML+= `<div class="empty"></div> `
-   }
- }
+      } else if(garden[r][c]== RAT){
+       div.innerHTML+= `<div class="rat" onclick="gather(RAT,${r},${c});"></div> `
+    }
+  }
 }
    div.innerHTML+= `<div class="coin"></div> `
    div_money.innerHTML =`Money: ${money}`
@@ -62,7 +108,11 @@ function gather(vegetable,r,c){
         garden[r][c]=EMPTY;
         money=money+CUCUMBER_PRICE;
     //  alert(CUCUMBER_PRICE);
-     show();
+       show();
   }
 }
 show();
+
+// plasati o capcana de afishat
+// daca rat nimerste in capcana +100
+// var deat, true folse 
