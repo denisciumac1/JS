9373d51ec1445b43ffaaf0d3f38804e3f1a5f7e6
denisciumac1 <43856379+denisciumac1@users.noreply.github.com>
2019-03-14 13:21:28 +0200

    Add files via upload

--- Garden-array/garden.js
+++ Garden-array/garden.js
@@ -8,6 +8,7 @@ const CUCUMBER_PRICE  = 10;
 const MELON_PRICE  =    20;
 const TOMATO_PRICE =    30;
 
+var money = 100;
 
 var garden = [
   MELON,     //0
@@ -24,30 +25,39 @@ var garden = [
 
 function show(){
   var div = document.getElementById('garden');
-  div.innerHTML ='';
+  var money = document.getElementById('money');
+  div.innerHTML =``;
   for(var i=0; i<=9; i++){
     if(garden[i]== MELON) {
-    div.innerHTML+= `<div class="melon" onclick="gather(MELON);"></div> `
+    div.innerHTML+= `<div class="melon" onclick="gather(MELON,'+i+');"></div> `
       }else if(garden[i]== TOMATO){
-    div.innerHTML+= `<div class="tomato" onclick="gather(TOMATO);"></div> `
+    div.innerHTML+= `<div class="tomato" onclick="gather(TOMATO,'+i+');"></div> `
       }else if(garden[i]== CUCUMBER){
-    div.innerHTML+= `<div class="cucumber" onclick="gather(CUCUMBER);"></div> `
+    div.innerHTML+= `<div class="cucumber" onclick="gather(CUCUMBER,'+i+');"></div> `
       }else if(garden[i]== EMPTY) {
     div.innerHTML+= `<div class="empty"></div> `
    }
-  }
+}
+   div.innerHTML+= `<div class="coin"></div> `
+   money.innerHTML =`Money: ${money}`
+
 }
 
-function gather(vegetable){
+
+
+function gather(vegetable, i){
   if(vegetable ==MELON){
-  alert(MELON_PRICE);
-}else if(vegetable == TOMATO){
-   alert(TOMATO_PRICE);
- } else if(vegetable == CUCUMBER){
- alert(CUCUMBER_PRICE);
+    garden[i]=EMPTY;
+
+  // alert(MELON_PRICE);
+    }else if(vegetable == TOMATO){
+      garden[i]=EMPTY;
+       // alert(TOMATO_PRICE);
+     } else if(vegetable == CUCUMBER){
+        garden[i]=EMPTY;
+     // alert(CUCUMBER_PRICE);
 
   }
 }
-
 
 show();
