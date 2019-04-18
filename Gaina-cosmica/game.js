e03b7b89d905895fa055c188f778c7031179c1dd
denisciumac1 <43856379+denisciumac1@users.noreply.github.com>
2019-04-18 19:46:39 +0300

    Add files via upload

---
+++ Gaina-cosmica/game.js
@@ -0,0 +1,4 @@
+let i = new Invader();
+i.addToMap();
+let p = new Player();
+p.addToMap();
---
+++ Gaina-cosmica/js/invader.js
@@ -0,0 +1,80 @@
+class Invader{
+  constructor(){
+    this.id = "i-" + Math.round(Math.random()*10000000);
+    this.type ='hombrew';
+    this.frames = ["0px -409px", "0px -430px;"]; //cadrele spritului
+    this.x = 0; //random() 0..width window
+    this.y = 0;
+    this.w = 52;
+    this.h = 30;
+    this.direction_h =  "right";   //
+  }
+
+
+   addToMap(){  //adaugam elementul pe harta(redesenam elementul)
+     window.map.innerHTML +=
+      `<div id ="${this.id}">` +
+       this.render()           +
+       `</div>`
+       this.live();
+   }
+
+
+   refresh(){
+     document.getElementById(this.id).innerHTML = this.render();
+    }
+
+  live(){
+    var that =this;
+    setInterval(function(){
+      that.frames.push(that.frames.shift());
+
+      if(that.direction_h == "right"){
+        that.moveRight();
+      }
+      if(that.direction_h == "left"){
+        that.moveLeft();
+      }
+
+      that.refresh();
+    },5 );
+  }
+
+
+  // movement
+  moveRight(){
+
+    if(this.x>window.innerWidth-this.w){
+      this.direction_h="left";
+      this.moveDown();
+    }
+    this.x +=5;
+  }
+
+  moveLeft(){
+
+    if(this.x<=0){
+      this.direction_h="right";
+      this.moveDown();
+    }
+    this.x -=5;
+  }
+
+  moveDown(){
+    this.y+=10;
+  }
+
+
+  render(){
+    let html = `
+    <div class="invader" style="width:  ${this.w}px;
+                                height: ${this.h}px;
+                                left:   ${this.x}px;
+                                top:    ${this.y}px;
+                                background-position:${this.frames[0]};
+    "></div>
+    `;
+    return html;
+  }
+
+}
