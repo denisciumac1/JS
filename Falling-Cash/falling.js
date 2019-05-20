13e394328b8bb2b3aef4c4aa48dd03229ea35785
denisciumac1 <43856379+denisciumac1@users.noreply.github.com>
2019-05-20 19:57:26 +0300

    Add files via upload

--- Falling-Cash/falling.js
+++ Falling-Cash/falling.js
@@ -1,14 +1,14 @@
-var stage = document.getElementById('stage');
-
 
 function addCoin(){
 // stage.innerHTML +=
 //   `<div class="coin">
 //      <img src="coin.png">
 //   </div>`;
+  var stage = document.getElementById('stage');
   var div = document.createElement('div');
   var img = document.createElement('img');
   div.className ="coin falling";
+  div.id = "elem";
   img.className ="rotating";
   div.style.left= Math.round(Math.random()*100)+ '%';
   div.style.transform= 'scale('+(Math.random()*1.5 + 0.2)+')';
@@ -22,9 +22,23 @@ function addCoin(){
   stage.appendChild(div);
 }
 
-function removeCoin(){
+var cash_score = 0;
+document.getElementById("stage").addEventListener("click", function removeCoin() {
+  var parent = document.getElementById('stage');
+    var elem = document.getElementById('elem');
+      parent.removeChild(elem);
+        document.body.onclick=false;
+           cash_score++;
 
+collectMoney();
+});
+
+
+function collectMoney(){
+var cash = document.getElementById('money');
+cash.innerHTML = `Cash:${cash_score}$`;
 }
+collectMoney();
 // // clic modeda
 // 1-ea dispare removechild
 // 2-creste cantitatea
