4c2f9b0b362518af481a7d57c1339bccea1a4e1a
denisciumac1 <43856379+denisciumac1@users.noreply.github.com>
2019-05-30 19:59:29 +0300

    Add files via upload

--- Dom-shop/shop.js
+++ Dom-shop/shop.js
@@ -31,11 +31,11 @@ var p = document.createElement('p');
 var a = document.createElement('a');
             a.className ="btn btn-primary stretched-link";
             a.innerText="Cumpara";
-            a.href = product.id;
+            a.href = `#${product.id}`;
             card_body.appendChild(a);
 
 
-  var row = document.querySelector("#products .row");
+  var row = document.querySelector("#products .row2");
   row.appendChild(card);
 
   a.addEventListener('click', addToCart);
@@ -44,14 +44,72 @@ var a = document.createElement('a');
 }
 
 function addToCart(e){
-  // //index -> dom-ul unui products
-  // e.target -> a care a fost clickat
-  // <a href ="#p1"  ---->localStorage
+  let product_id =  e.target.href.split("#").pop();
+
+  if(localStorage.getItem('cart')!=null){
+    var products_cart = localStorage.getItem('cart').split(",");
+  }else{
+    var products_cart = [];
+  }
+
+  products_cart.push(product_id);
+  localStorage.setItem('cart', products_cart);
+
+  showMessage("Your product was added to cart!")
 }
+function showMessage(){
+
+var message = document.createElement('div');
+    message.className ="alert alert-warning alert-dismissible fade show";
+
+var strong= document.createElement('strong');
+          strong.innerText="Success!";
+         message.appendChild(strong);
+let x  = document.createTextNode("Aici mesage.");
+         message.appendChild(x);
+
+var button= document.createElement('button');
+          button.className="close" ;
+          button.setAttribute('data-dismiss',"alert");
+           message.appendChild(button);
+
+var span= document.createElement('span');
+          span.innerHTML=`&times;`;
+          button.appendChild(span);
+          span.setAttribute('aria-hidden',"true");
+var products = document.querySelector("#products");
+            products.appendChild(message);
+
+setTimeout(function(){
+   products.removeChild(message);
+  } ,5000);
+}
+
 
 function renderProductList(){
     for(var i=0; i<products.length; i++){
         renderProduct(products[i]);
     }
 }
+
+function showCart(){
+
+  if(localStorage.getItem('cart')!=null){
+    var products_cart = localStorage.getItem('cart').split(",");
+  }else{
+    var products_cart = [];
+  }
+
+  var a = document.createElement('a');
+              a.className ="btn btn-primary btn-lg rounded-pill";
+              a.href = `#`;
+  var i = document.createElement('i');
+              i.className ="fas fa-shopping-cart";
+              a.appendChild(i);
+    let z  = document.createTextNode(`items:${products_cart.length}`);
+               a.appendChild(z);
+  var cart = document.querySelector("#products .py-5 .col-2");
+        cart.appendChild(a);
+}
+  showCart();
   renderProductList();
